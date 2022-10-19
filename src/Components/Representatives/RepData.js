e0a7a0c785ea31e63600ec18f7d1fb57bddba608
guna pranith <pranithreddyguna01@gmail.com>
2022-10-19 10:50:05 +0530

    completed all the api calls

--- src/Components/Representatives/RepData.js
+++ src/Components/Representatives/RepData.js
@@ -1,5 +1,5 @@
 // import { set } from "immer/dist/internal";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Attendence from "../../UI/Attendence";
 import Report from "../../UI/Report";
@@ -8,19 +8,21 @@ import AllRetailer from "../Retailers/AllRetailer";
 import RepReport from "./RepReport";
 import TableList from "./TableList";
 import "./RepData.css";
-
+import "../Retailers/AllRetailer.css";
+import { useParams } from "react-router-dom";
+import RepRet from "./RepRet";
 const RepData = () => {
   const [curPage, setCurPage] = useState("report");
   const [dates, setDates] = useState([]);
   const token = useSelector((state) => state.users.token);
-
-  const reportOpenHandler = () => {
-    setCurPage("report");
-  };
-  const attendanceOpenHandler = () => {
-    async function fetchAttendance() {
+  const {repid} = useParams();
+  const [req_data,setreqData]= useState([])
+  
+  useEffect(() => {
+    console.log("print inside useeffect");
+    async function repdata() {
       const response = await fetch(
-        "http://192.168.29.12:8080/api/v1/representatives/attendence/all",
+        "http://localhost:8080/api/v1/manager/representatives/"+repid,
         {
           method: "GET",
           headers: {
@@ -29,16 +31,26 @@ const RepData = () => {
           },
         }
       );
-      const data = response.json();
-      setDates(data)
+      const data =await  response.json();
+      console.log(data);
+      setreqData(data)
     };
-    fetchAttendance();
-    
+    repdata();
+  
+    return () => {
+      
+    }
+  }, [])
+  const reportOpenHandler = () => {
+    setCurPage("report");
+  };
+  const attendanceOpenHandler = () => {
     setCurPage("attendance");
   };
   const retailersOpenHandler = () => {
     setCurPage("retailers");
   };
+  console.log(req_data["attendances"],req_data["reports"],req_data["retailers"]);
   return (
     <DisplayFlex>
       <div className="retailerdatadiv">
@@ -66,11 +78,11 @@ const RepData = () => {
           </button>
         </div>
         <div style={{ width: "100%" }}>
-          {curPage === "report" && <Report type={"show"} />}
+          {curPage === "report" && <RepReport report ={req_data["reports"] ? req_data["reports"] : [] } type={"show"} />}
           {curPage === "attendance" && (
-            <Attendence dates={dates} type={"view"} />
+            <Attendence dates={req_data["attendances"]} type={"view"} />
           )}
-          {curPage === "retailers" && <AllRetailer />}
+          {curPage === "retailers" && <RepRet retailers = {req_data["retailers"]} />}
         </div>
       </div>
     </DisplayFlex>
--- src/Components/Representatives/RepReport.js
+++ src/Components/Representatives/RepReport.js
@@ -1,10 +1,35 @@
-// import React from 'react'
-// import Report from '../../UI/Report'
+import React from 'react'
+import Report from '../../UI/Report'
 
-// const RepReport = () => {
-//   return (
-    
-//   )
-// }
+const RepReport = (props) => {
+  console.log(props.report);
+  return (
+    <div className="divwithtable">
+    <table className="reqtable table table-hover">
+      <thead className="thead-dark" style={{ border: "0.5px solid black" }}>
+        <tr className="tableRow">
+          <th>id</th>
+          <th>Chemists Met</th>
+          <th>Chemists Onboarded</th>
+          <th>Previous Met</th>
+          <th>Orders</th>
+          <th>Comments</th>
+        </tr>
 
-// export default RepReport
+      </thead>
+      <tbody>
+        {props.report.map((data,index) => <tr style={{"textAlign":"center"}}>
+            <td>{index}</td>
+            <td>{data["metTotal"]}</td>
+            <td>{data["metNew"]}</td>
+            <td>{data["metOld"]}</td>
+            <td>{data["orders"]}</td>
+            <td style={{"maxwidth":"30px"}}>{data["comment"]["text"]}</td>
+        </tr>)}
+      </tbody>
+    </table>
+  </div>
+  )
+}
+
+export default RepReport
